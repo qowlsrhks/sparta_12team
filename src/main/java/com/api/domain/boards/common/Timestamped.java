5816efa0984d06a4cf18c3382d98bd94c2a348de
qowlsrhks <skswlsrhks@naver.com>
2024-09-02 20:54:51 +0900

    아직 상속하지 않음

---
+++ src/main/java/com/api/domain/boards/common/Timestamped.java
@@ -0,0 +1,25 @@
+package com.api.domain.boards.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    LocalDateTime modifiedAt;
+}
