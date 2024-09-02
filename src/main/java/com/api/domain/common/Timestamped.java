c9da585d715949a22cf99ba20be719344324f3c9
Bongwon <movieid@naver.com>
2024-09-02 15:34:30 +0900

    [Add] 유저 init, 최초 생성

---
+++ src/main/java/com/api/domain/common/Timestamped.java
@@ -0,0 +1,26 @@
+package com.api.domain.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    LocalDateTime modifiedAt;
+}
