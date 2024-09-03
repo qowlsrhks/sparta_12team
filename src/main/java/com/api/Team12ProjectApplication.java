333d4408674a1d76774c74306178904730f44789
durururuk <zkdltms0913@gmail.com>
2024-09-03 13:28:50 +0900

    [Add] "스프링 시큐리티 설정파일"
    스프링 시큐리티를 활성화 하기 위해 설정 파일을 추가했습니다.

--- src/main/java/com/api/Team12ProjectApplication.java
+++ src/main/java/com/api/Team12ProjectApplication.java
@@ -3,8 +3,10 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
+@EnableJpaAuditing
 public class Team12ProjectApplication {
 
     public static void main(String[] args) {
