e071332c4ae5058e9ef971ff9f091f9ed4424f44
qowlsrhks <skswlsrhks@naver.com>
2024-09-05 16:03:33 +0900

    임시 entity

---
+++ src/main/java/com/api/domain/boards/entity/Friend.java
@@ -0,0 +1,29 @@
+package com.api.domain.boards.entity;
+
+import com.api.domain.users.entity.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long friendId;
+
+    private String friendName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_user_id")
+    @JsonBackReference
+    private User friendUser;
+
+}
